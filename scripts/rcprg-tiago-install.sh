ddd463e8428af129e5cbed676a165ccb8bec50cc
Wojciech Dudek <wojciechsbox@gmail.com>
2020-01-20 19:05:16 +0100

    adds rcprg tiago install script

---
+++ scripts/rcprg-tiago-install.sh
@@ -0,0 +1,10 @@
+mkdir ~/tiago_public_ws
+cd ~/tiago_public_ws
+wget https://raw.githubusercontent.com/RCPRG-ros-pkg/tiago_sim_integration/master/rcprg_tiago_public-melodic.rosinstall
+sudo apt install python-rosinstall
+rosinstall src /opt/ros/melodic rcprg_tiago_public-melodic.rosinstall
+sudo apt install ros-melodic-imu-sensor-controller ros-melodic-force-torque-sensor-controller ros-melodic-twist-mux ros-melodic-four-wheel-steering-controller ros-melodic-image-proc ros-melodic-range-sensor-layer ros-melodic-pointcloud-to-laserscan ros-melodic-laser-filters ros-melodic-teb-local-planner ros-melodic-gmapping ros-melodic-octomap-server ros-melodic-octomap-mapping ros-melodic-people-msgs ros-melodic-rosbridge-server ros-melodic-rospy-message-converter bison flex
+cp src/rcprg/tiago_sim_integration/move_group_patch/move_group.launch src/pal/tiago_moveit_config/launch/move_group.launch
+cd ~/tiago_public_ws
+source /opt/ros/melodic/setup.bash
+catkin build -DCATKIN_ENABLE_TESTING=0
